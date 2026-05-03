5556695dc57d2c0d60a96655aa6575b9046e24dc
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-05-03 10:00:00 +0530

    Add typesafe parsing for environment variables

---
+++ backend-elysia/src/schemas/env.ts
@@ -0,0 +1,10 @@
+import z from 'zod';
+
+export const envSchema = z.object({
+  JWT_SECRET: z
+    .string({ error: 'JWT should be valid string.' })
+    .min(32, { error: 'JWT should be atleast 32 characters.' }),
+  JWT_EXPIRES_IN: z.coerce.number().default(60 * 60 * 1000),
+  NODE_ENV: z.enum(['PRODUCTION', 'DEV']).default('DEV'),
+  PORT: z.coerce.number().default(3000)
+});
