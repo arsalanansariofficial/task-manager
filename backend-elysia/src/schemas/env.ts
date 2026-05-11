5b79ba7e21a1f0bbcf1794c7b84f014843a9cf9e
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-05-11 10:00:00 +0530

    Add prisma and update env schema

--- backend-elysia/src/schemas/env.ts
+++ backend-elysia/src/schemas/env.ts
@@ -4,6 +4,7 @@ export const envSchema = z.object({
   JWT_SECRET: z
     .string({ error: 'JWT should be valid string.' })
     .min(32, { error: 'JWT should be atleast 32 characters.' }),
+  DATABASE_URL: z.url({ error: 'DATABASE_URL should be valid url.' }),
   JWT_EXPIRES_IN: z.coerce.number().default(60 * 60 * 1000),
   NODE_ENV: z.enum(['PRODUCTION', 'DEV']).default('DEV'),
   PORT: z.coerce.number().default(3000)
