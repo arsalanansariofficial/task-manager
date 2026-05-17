1bef09ffab98cd66e8f48d9bbb6e0280205133b1
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-05-17 10:00:00 +0530

    Add token model

---
+++ backend-elysia/src/prisma/migrations/20260725105730_swtich_to_mariadb/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE `UserProfile` (
+    `phoneNumber` VARCHAR(191) NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `imageUrl` VARCHAR(191) NULL,
+    `coverUrl` VARCHAR(191) NULL,
+    `bio` VARCHAR(191) NULL,
+    `address` VARCHAR(191) NULL,
+    `gender` ENUM('male', 'female') NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    UNIQUE INDEX `UserProfile_phoneNumber_key`(`phoneNumber`),
+    PRIMARY KEY (`userId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `verifiedAt` DATETIME(3) NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- CreateTable
+CREATE TABLE `Token` (
+    `token` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    UNIQUE INDEX `Token_token_key`(`token`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- AddForeignKey
+ALTER TABLE `UserProfile`
+ADD CONSTRAINT `UserProfile_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE `Token`
+ADD CONSTRAINT `Token_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
