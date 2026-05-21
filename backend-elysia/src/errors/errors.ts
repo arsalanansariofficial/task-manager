a9561f24fca5405aea1f37ca8002858ee42241d3
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-05-21 10:00:00 +0530

    Add error for invalid credentials

--- backend-elysia/src/errors/errors.ts
+++ backend-elysia/src/errors/errors.ts
@@ -1,3 +1,9 @@
+export class InvalidCredentialsError extends Error {
+  public override message = 'Either email or password is invalid.';
+  public path = ['email', 'password'];
+  public status = 400;
+}
+
 export class InvalidJwtError extends Error {
   public override message = 'Either jwt invalid or expired.';
   public path = ['jwt'];
