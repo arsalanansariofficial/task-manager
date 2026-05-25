9d2ae5227fb6cdb4f35f4aad85037669e2329337
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-05-25 10:00:00 +0530

    Add login and logout routes

--- backend-elysia/src/routes/user.ts
+++ backend-elysia/src/routes/user.ts
@@ -3,8 +3,10 @@ import { Elysia } from 'elysia';
 import {
   usersResponseSchema,
   userResponseSchema,
+  loginPayloadSchema,
   userPayloadSchema
 } from '@/schemas/user';
+import { successResponseSchema } from '@/schemas/generic';
 import * as userService from '@/services/user';
 import { jwtSchema } from '@/schemas/cookie';
 
@@ -14,6 +16,24 @@ router.post(
   '/',
   async ({ cookie: { jwt }, body }) => await userService.create(body, jwt),
   { response: userResponseSchema, body: userPayloadSchema, cookie: jwtSchema }
+);
+
+router.post(
+  '/login',
+  async ({ cookie: { jwt }, body }) => await userService.login(body, jwt),
+  { response: userResponseSchema, body: loginPayloadSchema, cookie: jwtSchema }
+);
+
+router.post(
+  '/logout',
+  async ({ cookie: { jwt } }) => await userService.logout(jwt),
+  { response: successResponseSchema, cookie: jwtSchema }
+);
+
+router.post(
+  '/logout/all',
+  async ({ cookie: { jwt } }) => await userService.logoutAll(jwt),
+  { response: successResponseSchema, cookie: jwtSchema }
 );
 
 router.get('/', async ({ cookie: { jwt } }) => await userService.get(jwt), {
--- backend-elysia/src/services/user.ts
+++ backend-elysia/src/services/user.ts
@@ -2,18 +2,24 @@ import type { Cookie } from 'elysia/cookies';
 
 import bcrypt from 'bcryptjs';
 
-import type { UserPayload } from '@/types/user';
+import type { loginPayload, UserPayload } from '@/types/user';
 
+import { InvalidCredentialsError } from '@/errors/errors';
 import { generateToken, verifyToken } from '@/lib/token';
 import { prisma } from '@/lib/prisma';
 import { env } from '@/config/env';
 
-export async function create(payload: UserPayload, jwt?: Cookie<unknown>) {
-  const user = await prisma.user.create({
-    data: { ...payload, password: await bcrypt.hash(payload.password, 8) },
-    omit: { password: true }
-  });
-
+export async function authenticate(
+  user: {
+    verifiedAt: Date | null;
+    createdAt: Date;
+    updatedAt: Date;
+    email: string;
+    name: string;
+    id: string;
+  },
+  jwt: Cookie<unknown> | undefined
+) {
   const { token } = await prisma.token.create({
     data: { token: generateToken(user.id), userId: user.id },
     select: { token: true }
@@ -34,6 +40,41 @@ export async function create(payload: UserPayload, jwt?: Cookie<unknown>) {
   });
 
   return user;
+}
+
+export async function login(
+  { password, email }: loginPayload,
+  jwt?: Cookie<unknown>
+) {
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user || !(await bcrypt.compare(password, user.password)))
+    throw new InvalidCredentialsError();
+
+  return await authenticate(user, jwt);
+}
+
+export async function create(payload: UserPayload, jwt?: Cookie<unknown>) {
+  const user = await prisma.user.create({
+    data: { ...payload, password: await bcrypt.hash(payload.password, 8) },
+    omit: { password: true }
+  });
+
+  return await authenticate(user, jwt);
+}
+
+export async function logoutAll(token?: Cookie<unknown>) {
+  const { id } = verifyToken(token?.value as string);
+  await prisma.token.deleteMany({ where: { userId: id } });
+  token?.remove();
+  return { message: 'All sessions has been revoked.', success: true };
+}
+
+export async function logout(token?: Cookie<unknown>) {
+  verifyToken(token?.value as string);
+  await prisma.token.delete({ where: { token: token?.value as string } });
+  token?.remove();
+  return { message: 'User has been logged out.', success: true };
 }
 
 export async function get(token?: Cookie<unknown>) {
