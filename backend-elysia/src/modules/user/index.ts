de46d3616b43799c7642aa4304ead62dd12d1926
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-06-08 10:00:00 +0530

    Update userService to service

--- backend-elysia/src/modules/user/index.ts
+++ backend-elysia/src/modules/user/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from 'elysia';
 
-import * as userService from '@/modules/user/service';
+import * as service from '@/modules/user/service';
 import { model } from '@/modules/user/model';
 import { auth } from '@/utils/auth';
 
@@ -10,12 +10,12 @@ router.group('', { cookie: model.jwt }, app =>
   app
     .post(
       '/',
-      async ({ cookie: { jwt }, body }) => await userService.create(body, jwt),
+      async ({ cookie: { jwt }, body }) => await service.create(body, jwt),
       { response: model.userResponse, body: model.userRequest }
     )
     .post(
       '/login',
-      async ({ cookie: { jwt }, body }) => await userService.login(body, jwt),
+      async ({ cookie: { jwt }, body }) => await service.login(body, jwt),
       { response: model.userResponse, body: model.loginRequest }
     )
 );
@@ -24,22 +24,20 @@ router.group('', app =>
   app
     .use(auth)
     .get('/me', ({ user }) => user, { response: model.userProfileResponse })
-    .delete('/me', async ({ user }) => await userService.deleteUser(user.id), {
+    .delete('/me', async ({ user }) => await service.deleteUser(user.id), {
       response: model.userResponse
     })
     .patch(
       '/me',
-      async ({ user, body }) => await userService.update(user.id, body),
+      async ({ user, body }) => await service.update(user.id, body),
       { response: model.userProfileResponse, body: model.userProfileRequest }
     )
-    .post(
-      '/logout',
-      async ({ cookie: { jwt } }) => await userService.logout(jwt),
-      { response: model.logoutResponse }
-    )
+    .post('/logout', async ({ cookie: { jwt } }) => await service.logout(jwt), {
+      response: model.logoutResponse
+    })
     .post(
       '/logout/all',
-      async ({ cookie: { jwt } }) => await userService.logoutAll(jwt),
+      async ({ cookie: { jwt } }) => await service.logoutAll(jwt),
       { response: model.logoutResponse }
     )
 );
