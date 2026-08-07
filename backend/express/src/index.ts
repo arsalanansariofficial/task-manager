1c6d37967addb0848c5cd36cc61ce0308b0c454b
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-08-07 10:00:00 +0530

    Connect db at app startup

--- backend/express/src/index.ts
+++ backend/express/src/index.ts
@@ -1,19 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import { connect } from 'mongoose';
 
-import connect from '@/lib/mongoose';
 import { env } from '@/lib/config';
 
-import userRouter from './modules/routers/user-router';
-import taskRouter from './modules/routers/task-router';
-
-connect();
-
-const app = express()
-  .use(express.static(env.UPLOAD_DIR))
-  .use(express.json())
-  .use(userRouter)
-  .use(taskRouter)
-  .use(cors());
-
-app.listen(env.PORT);
+connect(env.DATABASE_URL);
