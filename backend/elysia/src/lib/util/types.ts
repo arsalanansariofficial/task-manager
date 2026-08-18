ca07e35c1b4138d3f432f60a717eb9e100d27ae9
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-08-18 17:06:13 +0530

    Remove unused types

--- backend/elysia/src/lib/util/types.ts
+++ backend/elysia/src/lib/util/types.ts
@@ -1,20 +1,8 @@
 import z from 'zod';
 
-import type { Prisma } from '~/generated/prisma/client';
-import type { Model } from '@/modules/user/model';
+import type { none } from '@/lib/util';
 
-export type UserAndPayload = {
-  user: RequireFields<
-    Prisma.UserGetPayload<{
-      include: { profile: true; tokens: true };
-      omit: { password: true };
-    }>,
-    'email' | 'id'
-  >;
-  payload: Model['userProfilePayload'];
-};
 export type RequireFields<T, K extends keyof T> = Required<Pick<T, K>> & T;
 export type ModelType<T> = { [k in keyof T]: z.infer<T[k]> };
-export type Err = { path: Array<string>; message: string };
-export const none = z.union([z.null(), z.undefined()]);
+export type Err = { message: string; path: string[] };
 export type None = z.infer<typeof none>;
