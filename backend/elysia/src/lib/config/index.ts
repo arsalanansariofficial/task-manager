ab1031471dd8a796e3822bdb4087550752da3ab4
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-08-18 16:55:42 +0530

    Add better-auth environment variables with github oAuth

--- backend/elysia/src/lib/config/index.ts
+++ backend/elysia/src/lib/config/index.ts
@@ -1,9 +1,24 @@
 import z from 'zod';
 
 export const envSchema = z.object({
+  BETTER_AUTH_ACCEPT_METHODS: z
+    .tuple([
+      z.string({ error: 'Method name should be valid string.' }),
+      z.string({ error: 'Method name should be valid string.' })
+    ])
+    .default(['POST', 'GET']),
+  BETTER_AUTH_SECRET: z
+    .string({ error: 'BETTER_AUTH_SECRET should be valid string.' })
+    .min(32, { error: 'BETTER_AUTH_SECRET should be atleast 32 characters.' }),
   JWT_SECRET: z
     .string({ error: 'JWT should be valid string.' })
     .min(32, { error: 'JWT should be atleast 32 characters.' }),
+  GITHUB_CLIENT_SECRET: z
+    .string({ error: 'GITHUB_CLIENT_SECRET should be valid string.' })
+    .optional(),
+  GITHUB_CLIENT_ID: z
+    .string({ error: 'GITHUB_CLIENT_ID should be valid string.' })
+    .optional(),
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
