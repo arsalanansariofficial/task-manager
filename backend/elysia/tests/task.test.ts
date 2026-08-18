a1bc3780b8f7cfc54a8b9ab8f503e79035d766ca
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-08-18 17:07:14 +0530

    Update task.test.ts to use better-auth

--- backend/elysia/tests/task.test.ts
+++ backend/elysia/tests/task.test.ts
@@ -1,41 +1,67 @@
 import { beforeEach, afterAll, expect, test } from 'bun:test';
 import { HttpStatusCode } from 'axios';
 
-import { cleanupDb, setupDb, kevin, gwen, api, ben } from '@/tests/fixtures/db';
-import { generateToken } from '@/lib/token';
-import { prisma } from '@/lib/prisma';
+import type { Task } from '~/generated/prisma/client';
 
+import {
+  getSessionCookie,
+  resetDisk,
+  setupDb,
+  resetDb,
+  kevin,
+  gwen,
+  api,
+  ben
+} from '@/tests/fixtures/db';
+import { prisma } from '@/lib/prisma';
+import { auth } from '@/lib/auth';
+
+afterAll(async () => {
+  await Promise.all([resetDb(), resetDisk()]);
+  await prisma.$disconnect();
+});
 beforeEach(setupDb);
-afterAll(cleanupDb);
 
 test('should create task for user', async () => {
+  const { headers } = await auth.api.signInEmail({
+    returnHeaders: true,
+    body: { ...gwen }
+  });
+
   const { status, data } = await api.tasks.post(
     { title: 'Work with Charm Caster' },
-    { headers: { Cookie: `jwt=${generateToken(gwen.id as string)}` } }
+    getSessionCookie(headers)
   );
 
   const task = await prisma.task.findUnique({ where: { id: data?.id } });
   expect(task?.status).toBe('incomplete');
-  expect(task).not.toBeNull();
   expect(status).toBe(HttpStatusCode.Ok);
+  expect(task).not.toBeNull();
 });
 
 test('ben should not delete task created by kevin', async () => {
-  const [$task] = kevin.tasks;
-  const { status } = await api
-    .tasks({ id: $task!.id })
-    .delete(undefined, { headers: { Cookie: `jwt=${generateToken(ben.id)}` } });
+  const [$task] = kevin.tasks as [Task];
+  const { headers } = await auth.api.signInEmail({
+    returnHeaders: true,
+    body: { ...ben }
+  });
 
-  const task = await prisma.task.findUnique({ where: { id: $task?.id } });
-  expect(task).not.toBeNull();
+  const { status } = await api
+    .tasks({ id: $task.id })
+    .delete(undefined, getSessionCookie(headers));
+
+  const task = await prisma.task.findUnique({ where: { id: $task.id } });
   expect(status).toBe(HttpStatusCode.BadRequest);
+  expect(task).not.toBeNull();
 });
 
 test('should fetch tasks for user', async () => {
-  const { status, data } = await api.tasks.get({
-    headers: { Cookie: `jwt=${generateToken(gwen.id as string)}` }
+  const { headers } = await auth.api.signInEmail({
+    returnHeaders: true,
+    body: { ...gwen }
   });
 
-  expect(data?.length).toBe(1);
+  const { status, data } = await api.tasks.get(getSessionCookie(headers));
   expect(status).toBe(HttpStatusCode.Ok);
+  expect(data?.length).toBe(1);
 });
